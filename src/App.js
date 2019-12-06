770281a2bd1bc1bfd86e5ecc9a443f0e74c35b48
ouniemna <ouni.emna@hotmail.fr>
2019-12-06 12:14:51 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png'
+import VMC from './vmCard.png'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-card'>
+      <h1 class="grand-titre ">CREDIT CARD</h1>
+      <img className="puce" src={logo}></img>
+
+      <div className="container">
+
+        <div className="column">
+
+          <p className="num-1">7253  3256  7895  1245</p>
+          <p className="t-date">MONTH/YEAR</p>
+
+          <div className="l-2-horizontal">
+            <p className="n-date">5422</p>
+
+            <div className="box-right">
+              <p className="validation">VALID<br />THRU</p>
+              <i class="fas fa-caret-right"></i>
+              <p className="n-date">11/50</p>
+            </div>
+
+          </div>
+
+          <p className="sous-titre">CARDHOLDER</p>
+
+        </div>
+
+        <img className="visa" src={VMC}></img>
+
+      </div>
+
     </div>
   );
 }
+
 
 export default App;
